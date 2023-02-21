27ecacad82c787fe2adcb07a4fd5bb637dd1d5b4
Michaela Bačíková <bacikova.michaela@gmail.com>
2023-02-21 14:42:26 +0100

    prvy 5 selectov

--- 03-select.sql
+++ 03-select.sql
@@ -25,8 +25,55 @@ SELECT 5 * 3 as result;
 
 SELECT COUNT(*) FROM film_category;
 
+-- priklad vsetci zamestnanci a ich full adresy
+SELECT s.first_name as staff_name,
+       s.last_name as staff_surname,
+       a.address, c.city, a.postal_code, ct.country
+    FROM address a, city c, country ct, staff s
+    WHERE a.city_id = c.city_id
+    AND c.country_id = ct.country_id
+    AND s.address_id = a.address_id;
+
+-- 1
+SELECT
+  c.first_name, c.last_name,
+  ROUND(p.amount) as payment
+FROM
+  payment p, customer c
+WHERE
+  c.customer_id = p.customer_id
+  AND p.amount >= 10
+ORDER BY
+  p.amount DESC;
+
+-- 2 modifikovane - aj s nazvami filmov
+select f.title, c.name
+from
+    category c, film f, film_category fc
+where
+    fc.film_id = f.film_id
+    AND fc.category_id = c.category_id
+order by c.name;
+
+-- 3
 SELECT f.title, l.name FROM film f, language l
 WHERE f.language_id = l.language_id
 AND l.name = 'Japanese';
 
+-- 4 kolko akcnych filmov sa nachadza v nasej databaze? (64)
+select COUNT(f.*)
+from
+    category c, film f, film_category fc
+where
+    fc.film_id = f.film_id
+    AND fc.category_id = c.category_id
+    AND c.name LIKE 'Action';
 
+-- 5 vypiste pocet vsetkych filmov v obchode so store_id 1 (2270)
+select COUNT(f.*)
+from store s, film f, inventory i
+where i.film_id = f.film_id
+    and i.store_id = s.store_id
+    and s.store_id = 1;
+
+-- 6
